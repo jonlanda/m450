5e9085ad625444b8704f3d5acdd57cfadce5667f
Jon Landa <jonlanda06@gmail.com>
2024-01-25 15:19:40 +0100

    making tests better

--- footballStatComparer/src/test/java/PlayerStatsCalculatorServiceTest.java
+++ footballStatComparer/src/test/java/PlayerStatsCalculatorServiceTest.java
@@ -8,9 +8,13 @@ public class PlayerStatsCalculatorServiceTest {
 
     private PlayerStatsCalculatorService calculator;
 
+    @BeforeEach
+    public void setUp() {
+        calculator = new PlayerStatsCalculatorService();
+    }
+
     @Test
     public void testCalculatePoints() {
-        calculator = new PlayerStatsCalculatorService();
         PlayerStats playerStats = new PlayerStats(5, 3, 90, 2, 0, 8, 15);
         double expectedPoints = 62.89;
         double actualPoints = calculator.calculatePoints(playerStats);
@@ -19,7 +23,6 @@ public class PlayerStatsCalculatorServiceTest {
 
     @Test
     public void testCalculatePointsWithZeroMinutesPlayed() {
-        calculator = new PlayerStatsCalculatorService();
         PlayerStats playerStats = new PlayerStats(3, 2, 0, 1, 0, 5, 10);
         int expectedPoints = 0;
         double actualPoints = calculator.calculatePoints(playerStats);
@@ -28,7 +31,6 @@ public class PlayerStatsCalculatorServiceTest {
 
     @Test
     public void testCalculatePointsWithZeroStats() {
-        calculator = new PlayerStatsCalculatorService();
         PlayerStats playerStats = new PlayerStats(0, 0, 90, 0, 0, 0, 0);
         int expectedPoints = 9;
         double actualPoints = calculator.calculatePoints(playerStats);
@@ -37,7 +39,6 @@ public class PlayerStatsCalculatorServiceTest {
 
     @Test
     public void testCalculatePointsWithShotsPerGoal() {
-        calculator = new PlayerStatsCalculatorService();
         PlayerStats playerStats = new PlayerStats(4, 2, 120, 1, 0, 5, 20);
         double expectedPoints = 61.53;
         double actualPoints = calculator.calculatePoints(playerStats);
@@ -46,7 +47,6 @@ public class PlayerStatsCalculatorServiceTest {
 
     @Test
     public void testCalculatePointsForOneGoal() {
-        calculator = new PlayerStatsCalculatorService();
         PlayerStats playerStats = new PlayerStats(1, 0, 90, 0, 0, 0, 1);
         double expectedPoints = 16.11;
         double actualPoints = calculator.calculatePoints(playerStats);
@@ -55,7 +55,6 @@ public class PlayerStatsCalculatorServiceTest {
 
     @Test
     public void testComparePlayersAndDetermineWinner() {
-        calculator = new PlayerStatsCalculatorService();
         PlayerStats player1 = new PlayerStats(3, 2, 90, 1, 0, 5, 15);
         PlayerStats player2 = new PlayerStats(2, 3, 90, 0, 0, 7, 10);
 
